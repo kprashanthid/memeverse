369304ec37faf66e6a2c4f4ba03c90ff58c121b2
prashanth <prashanthkamalakannank@gmail.com>
2025-03-08 08:30:52 +0530

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { motion } from "framer-motion";
 import memeImage from "./meme.png";
+import Link from "next/link";
+import Image from "next/image";
 
 const memes = [
   {
@@ -27,25 +28,34 @@ export default function NotFoundPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
-      <motion.img
-        src={randomMeme.url}
-        alt="404 Meme"
-        className="w-full mt-6 rounded-lg shadow-lg"
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-      />
-
-      <p className="text-lg mt-4">{randomMeme.caption}</p>
-
-      <motion.button
-        onClick={() => router.push("/")}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="mt-6 px-6 py-3 bg-blue-500 text-white text-lg rounded-md shadow-md"
-      >
-        Go Back Home 🏠
-      </motion.button>
+      <section className="flex flex-col items-center justify-center min-h-screen bg-white font-serif p-10">
+        <div className="text-center">
+          <h1 className="text-8xl font-bold text-black inset-0 flex items-center justify-center">
+            404
+          </h1>
+          <Image
+            height={500}
+            width={500}
+            alt="Loading"
+            src={
+              "https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+            }
+          />
+          <div className="mt-[-50px]">
+            <h3 className="text-3xl font-semibold text-gray-800">
+              Look like you're lost
+            </h3>
+            <p className="text-gray-600 mt-2">
+              The page you are looking for is not available!
+            </p>
+            <Link href="/">
+              <button className="mt-4 px-6 py-3 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700 transition">
+                Go to Home
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dribbble.com",
+      },
+    ],
     domains: ["i.imgflip.com"],
   },
 };
